c57d8957049ad6fdc3ad583fd8568a5dd3e59f6f
endriocm <endriomeireles5@gmail.com>
2026-01-27 01:40:57 -0300

    update

--- electron/preload.js
+++ electron/preload.js
@@ -1,7 +1,3 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  selectFolder: () => ipcRenderer.invoke('select-folder'),
-  resolveFolder: (folderPath) => ipcRenderer.invoke('resolve-folder', folderPath),
-  readFile: (filePath) => ipcRenderer.invoke('read-file', filePath),
-})
+version https://git-lfs.github.com/spec/v1
+oid sha256:67694ebedf10bca892065b94f4018947086f6776ac01e88587893f0e2078f12b
+size 323
